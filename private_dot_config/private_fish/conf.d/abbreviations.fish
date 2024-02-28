4d605d5959821e4f82c1e1a5677de99c05b78cb2
Christian Romney <christian.a.romney@gmail.comsigningkey =A0FA17A0540AAC06[core]>
2024-02-28 15:36:23 -0500

    Update .gitconfig Add .config/emacs/bin/doom.cmd Add .config/emacs/.git/refs/tags/.keep Add .config/emacs/.git/refs/tags/v0.7 Add .config/emacs/.git/refs/tags/v0.9 Add .config/emacs/.git/refs/tags/v1.0 Add .config/emacs/.git/refs/tags/v1.1 Add .config/emacs/.git/refs/tags/v1.2 Add .config/emacs/.git/refs/tags/v1.3 Add .config/emacs/.git/refs/tags/v2.0 Add .config/emacs/.git/refs/tags/v2.0.3 Add .config/emacs/.git/refs/tags/v2.0.4 Add .config/emacs/.git/refs/tags/v2.0.5 Add .config/emacs/.git/refs/tags/v2.0.6 Add .config/emacs/.git/refs/tags/v2.0.7 Add .config/emacs/.git/refs/tags/v2.0.8 Add .config/emacs/.git/refs/tags/v2.0.9 Add .config/emacs/.local/cache/autosave/39645-mint.local~ Add .config/emacs/.local/cache/autosave/35865-mint.local~ Add .config/emacs/.local/cache/projectile/projectile.cache Add .config/emacs/.local/etc/@/init.29.d/05-init-vars.auto.el Add .config/emacs/.local/etc/@/init.29.d/80-loaddefs.auto.el Add .config/emacs/.local/etc/@/init.29.d/90-loaddefs-packages.auto.el Add .config/emacs/.local/etc/@/init.29.d/95-load-modules.auto.el Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/client/node_modules/vscode-jsonrpc/node.cmd Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/client/node_modules/vscode-languageclient/node.cmd Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/client/node_modules/vscode-languageserver-protocol/node.cmd Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/client/out/extension.js.map Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/server/node_modules/vscode-jsonrpc/node.cmd Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/server/node_modules/vscode-languageserver-protocol/node.cmd Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/server/node_modules/vscode-languageserver/node.cmd Add .config/emacs/.local/etc/lsp/eslint/unzipped/extension/server/out/eslintServer.js.map Add .config/emacs/.local/etc/plantuml.jar Add .config/fish/conf.d/.keep Add .config/fish/conf.d/abbr_tips.fish Add .config/fish/conf.d/abbreviations.fish Add .config/fish/conf.d/aliases.fish Add .config/fish/conf.d/autopair.fish

---
+++ private_dot_config/private_fish/conf.d/abbreviations.fish
@@ -0,0 +1,4 @@
+# kickoff websearch from CLI
+abbr --add goog s -p google
+abbr --add amzn s -p amazon
+abbr --add wiki s -p wikipedia
---
+++ private_dot_config/private_fish/conf.d/aliases.fish
@@ -0,0 +1,8 @@
+alias cm='chezmoi'
+alias cma='chezmoi add -q -r --secrets error'
+alias cms='chezmoi status'
+alias ls='eza'
+alias ll='eza -lah'
+alias rf='rm -rf'
+alias save-brews="list-brews | tee $HOME/.config/homebrew/formulae.txt"
+alias save-casks="list-casks | tee $HOME/.config/homebrew/casks.txt"

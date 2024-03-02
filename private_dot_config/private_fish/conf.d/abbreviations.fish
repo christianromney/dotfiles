873e9e3cd771955c0d0efe2961341c9b9473f92a
Christian Romney <christian.a.romney@gmail.com>
2024-03-02 08:47:12 -0500

    Update .config/fish/conf.d/abbreviations.fish

--- private_dot_config/private_fish/conf.d/abbreviations.fish
+++ private_dot_config/private_fish/conf.d/abbreviations.fish
@@ -2,3 +2,13 @@
 abbr --add goog s -p google
 abbr --add amzn s -p amazon
 abbr --add wiki s -p wikipedia
+abbr --add np new-project
+
+alias cm='chezmoi'
+alias cma='chezmoi add -q -r --secrets error'
+alias cms='chezmoi status'
+alias ls='eza'
+alias ll='eza -lah'
+alias rf='rm -rf'
+alias save-brews="list-brews | tee $HOME/.config/homebrew/formulae.txt"
+alias save-casks="list-casks | tee $HOME/.config/homebrew/casks.txt"

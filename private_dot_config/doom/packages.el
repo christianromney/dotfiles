ca93d627e7399076075e4c1059fc2c51699cba67
Christian Romney <christian.romney@nubank.com.br>
2025-07-09 12:07:00 -0400

    Update .config/doom/packages.el

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -1,8 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
+
 ;; ----------------------------------------------------------------------------------
 ;; Appearance
 ;; ----------------------------------------------------------------------------------
+
 (package! pulsar :recipe (:host github :repo "protesilaos/pulsar"))
 
 ;; ----------------------------------------------------------------------------------
@@ -10,15 +12,14 @@
 ;; ----------------------------------------------------------------------------------
 (package! adoc-mode)
 (package! gist :recipe (:host github :repo "KarimAziev/igist"))
-
-;; (package! inf-clojure)
 (when (modulep! :checkers syntax)
   (package! flycheck-clj-kondo))
 (package! clojure-snippets)
+
 ;; ----------------------------------------------------------------------------------
 ;; AI
 ;; ----------------------------------------------------------------------------------
-(package! ellama)  ;; general package for interacting with LLMs from Emacs
+
 (package! greader) ;; for text-to-speech
 (package! whisper :recipe (:host github :repo "natrys/whisper.el")) ;; speech-to-text
 (package! aidermacs)
@@ -30,6 +31,7 @@
 ;; ----------------------------------------------------------------------------------
 ;; Org
 ;; ----------------------------------------------------------------------------------
+
 (package! org-modern)
 (package! brazilian-holidays)
 (package! consult-org-roam :recipe(:host github :repo "jgru/consult-org-roam"))
@@ -38,9 +40,11 @@
 (package! graphviz-dot-mode) ;; graphviz diagrams
 (package! mermaid-mode) ;; even more diagrams
 (package! ob-mermaid)
+
 ;; ----------------------------------------------------------------------------------
 ;; Utilities
 ;; ----------------------------------------------------------------------------------
+
 (package! consult-company)
 (package! consult-yasnippet)
 (package! free-keys :recipe (:host github :repo "Fuco1/free-keys")) ;; find available
